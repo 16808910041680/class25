4f24825f626b1e115aaed5d48059eb1beaae873e
16808910041680 <forforfirefire@gmail.com>
2025-08-10 11:15:41 +1000

    hoiyy

--- pairelement.py
+++ pairelement.py
@@ -15,9 +15,23 @@ class pair_element:
         sum2 = sum(self.dict2)
         print("Sum of elements in dict1:", sum1)
         print("Sum of elements in dict2:", sum2)
+    def sum_elements_median(self):
+        sum1 = sum(self.dict1)
+        sum2 = sum(self.dict2)
+        sum1 /= len(self.dict1)
+        sum2 /= len(self.dict2)
+        sum1 = sum1 + 30
+        sum2 = sum2 + 30
+        match1 = 0
+        sum1 - self.dict1 == match1
+        print (sum1, match1, "Matches up to the last element in dict1.")
+
 element1 = pair_element({10,20,30,40,50,60,70}, {30,40,50,60,70,80,90})
 pair_element.pair_elements(element1)
 pair_element.sum_elements(element1)
+pair_element.sum_elements_median(element1)
+
+
 
 
 
