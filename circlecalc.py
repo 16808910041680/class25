6bd1afaa2af5d0fcfd9baf65e4acbbbf93f0e5ed
16808910041680 <forforfirefire@gmail.com>
2025-08-31 11:04:42 +1000

    gugojifdoua

---
+++ circlecalc.py
@@ -0,0 +1,4 @@
+pi = 3.14159
+radius = float(input("Enter the radius of the circle: "))
+print(f"The area of the circle with radius {radius} is",  pi * radius ** 2)
+print (f"The perimiter of the circle with radius {radius} is {2 * pi * radius:.2f}")
